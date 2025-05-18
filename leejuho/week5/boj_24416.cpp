b1e7daa4482d0609d2fb5c420745be9065d12875
ljh5271 <hma6191@gmail.com>
2025-05-18 16:14:22 +0900

    "(#15) [week5] leejuho"

---
+++ leejuho/week5/boj_24416.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n, int &count) {
+    if (n == 1 || n == 2) {
+        count++;
+        return 1;
+    } else {
+        return fib(n - 1, count) + fib(n - 2, count);
+    }
+}
+
+int fibonacci(int n, int &count_dp) {
+    int* dp = new int[n + 1];
+    dp[1] = dp[2] = 1;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+        count_dp++;
+    }
+    int result = dp[n];
+    delete[] dp;
+    return result;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int count_recursive = 0;
+    int count_dp = 0;
+
+    fib(n, count_recursive);
+    fibonacci(n, count_dp);
+
+    cout << count_recursive << " " << count_dp << '\n';
+    return 0;
+}
---
+++ leejuho/week5/boj_2579.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int stair[301] = {0}; // 계단 점수
+    int dp[301] = {0};    // dp[i] = i번째 계단까지의 최대 점수
+
+    for (int i = 1; i <= n; i++) {
+        cin >> stair[i];
+    }
+
+    dp[1] = stair[1];
+    dp[2] = stair[1] + stair[2];
+    dp[3] = max(stair[1] + stair[3], stair[2] + stair[3]);
+
+    for (int i = 4; i <= n; i++) {
+        dp[i] = max(dp[i - 2] + stair[i], dp[i - 3] + stair[i - 1] + stair[i]);
+    }
+
+    cout << dp[n] << '\n';
+    return 0;
+}
